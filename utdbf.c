8f740a92c8ae6f929c735fe206de0ce6b77b1348
Uyen Truong <haiuyentruong@gmail.com>
2019-02-21 09:55:59 +0200

    Add fields in array

--- utdbf.c
+++ utdbf.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void parse_header(FILE *fp);
-int  parse_field(FILE *fp);
-void parse_int32(unsigned char *buf, int *p, int n);
-void parse_int16(unsigned char *buf, short *p, int n);
+typedef struct header {
+    int   record_num;
+    short header_size;
+    short record_size;
+} Header;
+
+typedef struct field {
+    char          type;
+    unsigned char len;
+} Field;
+
+Header *parse_header(FILE *fp);
+Field  *parse_field(FILE *fp);
+void    print_nth_record(FILE *fp, Header *header, int n);
+void    parse_int32(unsigned char *buf, int *p, int n);
+void    parse_int16(unsigned char *buf, short *p, int n);
 
 int main(int argc, char **argv)
 {
@@ -15,18 +28,27 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    parse_header(fp);
+    Header *header = parse_header(fp);
+    int num_fields = (header->header_size - 33) / 32;
+    printf("Total number of fields\t%d\n", num_fields);
 
+    Field **array = malloc(sizeof(Field *) * num_fields);
     printf("\n>>>> Field descriptor array\n");
-    while (parse_field(fp) != -1)
-        ;
 
+    for (int i = 0; i < num_fields; i++)
+        array[i] = parse_field(fp);
+
+    for (int i = 0; i < num_fields; i++)
+        free(array[i]);
+    free(array);
+    free(header);
     fclose(fp);
     return 0;
 }
 
-void parse_header(FILE *fp)
+Header *parse_header(FILE *fp)
 {
+    Header *head = malloc(sizeof(Header));
     unsigned char header[32];
     int num;
     short head_record[2];
@@ -34,6 +56,10 @@ void parse_header(FILE *fp)
     fread(header, sizeof(header), 1, fp);
     parse_int32(header + 4, &num, 1);           // Parse num of records
     parse_int16(header + 8, head_record, 2);    // Parse num of bytes in header & record
+
+    head->record_num = num;
+    head->header_size = head_record[0];
+    head->record_size = head_record[1];
 
     printf("File type\t\t0x%02x\n", header[0]); //0x03 dBase IV memo file
     printf("YYYY MM DD\t\t%i %02i %02i\n", header[1] + 1900, header[2], header[3]);
@@ -51,37 +77,52 @@ void parse_header(FILE *fp)
         printf("(no MDX)\n");
     printf("Language driver ID     \t%02x\n", header[29]);
     // 2 bytes reserved
+
+    return head;
 }
 
-int parse_field(FILE *fp)
+Field *parse_field(FILE *fp)
 {
+    Field *fd = malloc(sizeof(Field));
     unsigned char field[32];
-    int i;
     fread(field, sizeof(field), 1, fp);
     if (field[0] == 0x0D)
-        return -1;
+        return NULL;
 
-    printf("\nField name                 \t");
-    for (i = 0; i < 11; i++)
+    fd->type = field[11];
+    fd->len  = field[16];
+
+    printf("\nField name       \t");
+    for (int i = 0; i < 11; i++)
         printf("%c", field[i]);
     printf("\n");
 
-    printf("Field type                   \t%c\n", field[11]);
+    printf("Field type         \t%c\n", field[11]);
     // 4 bytes reserved
-    printf("Field length in binary       \t%u\n", field[16]);
-    printf("Field decimal count in binary\t%u\n", field[17]);
+    printf("Field length       \t%u\n", field[16]);
+    printf("Field decimal count\t%u\n", field[17]);
     // 2 bytes reserved
-    printf("Work area ID                 \t0x%02x\n", field[20]);
+    printf("Work area ID        \t0x%02x\n", field[20]);
     // 10 bytes reserved
-    printf("MDX flag                     \t0x%02x ", field[31]);
+    printf("MDX flag            \t0x%02x ", field[31]);
     if (field[31] == 0x01)
         printf("(has index tag)\n");
     else
         printf("(no index tag)\n");
 
-    return 0;
+    return fd;
 }
 
+void print_nth_record(FILE *fp, Header *header, int n)
+{
+    int size = header->record_num * header->record_size;
+    unsigned char *records = malloc(size);
+    fread(records, sizeof(records), 1, fp);
+
+
+    printf("Total size %d\n", size);
+
+}
 
 void parse_int32(unsigned char *buf, int *p, int n)
 {
