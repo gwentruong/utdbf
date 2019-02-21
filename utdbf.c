ebb578a4e930fdd03d87cfd63266eeabb3a64398
Uyen Truong <haiuyentruong@gmail.com>
2019-02-21 12:37:55 +0200

    Add record to linkedlist, TODO: Fix reverse record bug

--- utdbf.c
+++ utdbf.c
@@ -12,15 +12,27 @@ typedef struct field {
     unsigned char len;
 } Field;
 
+typedef struct record {
+    unsigned char  is_deleted;
+    unsigned char *content;
+    struct record *next;
+} Record;
+
 Header *parse_header(FILE *fp);
 Field  *parse_field(FILE *fp);
-void    print_nth_record(FILE *fp, Header *header, int n);
+Record *parse_record(FILE *fp, Header *header);
 void    parse_int32(unsigned char *buf, int *p, int n);
 void    parse_int16(unsigned char *buf, short *p, int n);
+int     record_prepend(Record **p_head, Record *new_record);
+void    record_reverse(Record **p_head);
+void    record_free(Record *head);
 
 int main(int argc, char **argv)
 {
     char *filename = argv[1];
+    int   i;
+    int   m;
+    int len = 0;
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL)
     {
@@ -29,16 +41,32 @@ int main(int argc, char **argv)
     }
 
     Header *header = parse_header(fp);
-    int num_fields = (header->header_size - 33) / 32;
+    int     num_fields = (header->header_size - 33) / 32;
     printf("Total number of fields\t%d\n", num_fields);
 
     Field **array = malloc(sizeof(Field *) * num_fields);
     printf("\n>>>> Field descriptor array\n");
 
-    for (int i = 0; i < num_fields; i++)
+    for (i = 0; i < num_fields; i++)
+    {
         array[i] = parse_field(fp);
+        printf("XXXX array[%d] Type %c, len %i\n", i, array[i]->type, array[i]->len);
+    }
 
-    for (int i = 0; i < num_fields; i++)
+    Record *record = NULL;
+    while((m = record_prepend(&record, parse_record(fp, header))) == 0)
+    {
+        printf("XxXXXXXX %d\n", m);
+        len++;
+    }
+
+    printf("$$$$$$ Done parsing\n");
+    printf("###### Number of records %d\n", len);
+    record_reverse(&record);
+    printf("$$$$$$ Done reversing\n");
+
+    record_free(record);
+    for (i = 0; i < num_fields; i++)
         free(array[i]);
     free(array);
     free(header);
@@ -48,16 +76,16 @@ int main(int argc, char **argv)
 
 Header *parse_header(FILE *fp)
 {
-    Header *head = malloc(sizeof(Header));
+    Header       *head = malloc(sizeof(Header));
     unsigned char header[32];
-    int num;
-    short head_record[2];
+    int           num;
+    short         head_record[2];
 
     fread(header, sizeof(header), 1, fp);
     parse_int32(header + 4, &num, 1);           // Parse num of records
     parse_int16(header + 8, head_record, 2);    // Parse num of bytes in header & record
 
-    head->record_num = num;
+    head->record_num  = num;
     head->header_size = head_record[0];
     head->record_size = head_record[1];
 
@@ -83,8 +111,9 @@ Header *parse_header(FILE *fp)
 
 Field *parse_field(FILE *fp)
 {
-    Field *fd = malloc(sizeof(Field));
+    Field        *fd = malloc(sizeof(Field));
     unsigned char field[32];
+
     fread(field, sizeof(field), 1, fp);
     if (field[0] == 0x0D)
         return NULL;
@@ -113,15 +142,67 @@ Field *parse_field(FILE *fp)
     return fd;
 }
 
-void print_nth_record(FILE *fp, Header *header, int n)
+Record *parse_record(FILE *fp, Header *header)
 {
-    int size = header->record_num * header->record_size;
-    unsigned char *records = malloc(size);
-    fread(records, sizeof(records), 1, fp);
+    int           size = header->record_size;
+    Record       *rec = malloc(sizeof(size));
+    unsigned char record[size];
 
+    int nitems = fread(record, sizeof(record), 1, fp);
+    printf(">>>>>>>>>>>>>>>>nitem %02x\n", nitems);
+    if (nitems != 1)     // EOF
+        return NULL;
 
-    printf("Total size %d\n", size);
+    rec->is_deleted = record[0];
+    rec->next       = NULL;
 
+    rec->content    = malloc(size - 1);
+    for (int i = 1, j = 0; i < size; i++, j++)
+        rec->content[j] = record[i];
+
+    return rec;
+}
+
+int record_prepend(Record **p_head, Record *new_record)
+{
+    if (new_record == NULL)
+        return -1;
+
+    new_record->next = *p_head;
+    *p_head = new_record;
+
+    return 0;
+}
+
+void record_reverse(Record **p_head)
+{
+    Record *prev = NULL;
+    Record *current = *p_head;
+    Record *next;
+    int cnt = 0;
+    printf("Start reversing\n");
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        cnt++;
+        printf("Success %d\n", cnt);
+    }
+    printf("Done reversing\n");
+
+    *p_head = prev;
+}
+
+void record_free(Record *head)
+{
+    for (Record *p = head; p != NULL; p = head)
+    {
+        head = head->next;
+        free(p->content);
+        free(p);
+    }
 }
 
 void parse_int32(unsigned char *buf, int *p, int n)
